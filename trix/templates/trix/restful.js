6f0648470742ac58160f0d5a5f4223482779e1e4
jonathan <jwringstad@gmail.com>
2013-07-22 17:14:55 +0200

    finished restructuring topic view

--- trix/templates/trix/restful.js
+++ trix/templates/trix/restful.js
@@ -66,3 +66,47 @@ Ext.define('trix.apps.trix.simplified.subject.SimplifiedSubjectSearch', {
                 }
     })
 });
+
+Ext.define('trix.apps.trix.simplified.topic.SimplifiedTopic', {
+    extend: 'Ext.data.Model',
+    requires: ['devilry.extjshelpers.RestProxy'],
+    fields: [{"type": "int", "name": "id"}, {"type": "auto", "name": "name"}],
+    idProperty: 'id',
+    proxy: Ext.create('devilry.extjshelpers.RestProxy', {
+        url: '/trix/restfulsimplifiedtopic/',
+        extraParams: {
+            getdata_in_qrystring: true,
+            result_fieldgroups: '[]'
+        },
+        reader: {
+            type: 'json',
+            root: 'items',
+            totalProperty: 'total'
+        },
+        writer: {
+            type: 'json'
+        }
+    })
+});
+
+Ext.define('trix.apps.trix.simplified.exercise.SimplifiedExercise', {
+    extend: 'Ext.data.Model',
+    requires: ['devilry.extjshelpers.RestProxy'],
+    fields: [{"type": "int", "name": "id"}, {"type": "auto", "name": "short_name"}, {"type": "auto", "name": "long_name"}, {"type": "auto", "name": "text"}, {"type": "int", "name": "points"}, {"type": "int", "name": "topics__id"}, {"type": "int", "name": "prerequisites__id"}, {"type": "auto", "name": "fake_topics"}, {"type": "auto", "name": "fake_prerequisites"}],
+    idProperty: 'id',
+    proxy: Ext.create('devilry.extjshelpers.RestProxy', {
+        url: '/trix/restfulsimplifiedexercise/',
+        extraParams: {
+            getdata_in_qrystring: true,
+            result_fieldgroups: '[]'
+        },
+        reader: {
+            type: 'json',
+            root: 'items',
+            totalProperty: 'total'
+        },
+        writer: {
+            type: 'json'
+        }
+    })
+});
