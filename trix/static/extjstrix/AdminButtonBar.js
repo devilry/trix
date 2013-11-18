9dcec584a617fbb8e2a33e2d6c82b17871708d3e
Espen A. Kristiansen <post@espenak.net>
2013-11-18 21:12:30 +0100

    static...AdminButtonBar: Fixed indent and syntax errors.

--- trix/static/extjstrix/AdminButtonBar.js
+++ trix/static/extjstrix/AdminButtonBar.js
@@ -10,7 +10,7 @@ Ext.define('trix.AdminButtonBar', {
         'trix.forms.Exercise',
         // 'trix.forms.PeriodExercise',
         'devilry.administrator.DefaultCreateWindow',
-        'trix_extjshelpers.RestfulSimplifiedEditPanel',
+        'trix_extjshelpers.RestfulSimplifiedEditPanel'
         // 'trix_extjshelpers.ButtonBarButton'
     ],
     
@@ -23,8 +23,8 @@ Ext.define('trix.AdminButtonBar', {
         //padding: '40 40 40 40'
     //},
     config: {
-	topic_modelname: undefined,
-	exercise_modelname: undefined,
+        topic_modelname: undefined,
+        exercise_modelname: undefined
     },
     constructor: function(config){
 	this.initConfig(config);
@@ -33,60 +33,55 @@ Ext.define('trix.AdminButtonBar', {
     initComponent: function() {
         var me = this;
         Ext.apply(this, {
-            items: [
-		{
-		    xtype: 'container',
-		    layout: {
-			type: 'hbox',
-			pack: 'center',
-			align: 'center',
-		    },
-		    items: [
-			{
-			    xtype: 'button',
-                scale: 'large',
-			    text: "New Topic",
-			    handler: function() {
-				console.log("new topic! " + me.topic_modelname);
-				Ext.create('trix.DefaultEditWindow', {
-				    title: 'Create new topic',
-				    editpanel: Ext.ComponentManager.create({
-					xtype: 'restfulsimplified_editpanel',
-					model: me.topic_modelname,
-					editform: Ext.widget('administrator_topicform')
-				    }),
-				}).show();
-			    }
-			},
-			{
-			    xtype: 'button',
-                scale: 'large',
-			    text: "New Exercise",
-			    handler: function() {
-				Ext.create('trix.DefaultEditWindow', {
-				    title: 'Create new exercise',
-				    editpanel: Ext.ComponentManager.create({
-					xtype: 'restfulsimplified_editpanel',
-					model: me.exercise_modelname,
-					editform: Ext.widget('administrator_exerciseform')
-				    }),
-				}).show();
-			    }
-			}
-		    ]
-		},
-		{
-            xtype: 'box',
-            style: 'text-align: center;',
-            margin: '10px 0 0 0',
+            items: [{
+                xtype: 'container',
+                layout: {
+                    type: 'hbox',
+                    pack: 'center',
+                    align: 'center'
+                },
+                items: [{
+                    xtype: 'button',
+                    scale: 'large',
+                    text: "New Topic",
+                    handler: function() {
+                        console.log("new topic! " + me.topic_modelname);
+                        Ext.create('trix.DefaultEditWindow', {
+                            title: 'Create new topic',
+                            editpanel: Ext.ComponentManager.create({
+                                xtype: 'restfulsimplified_editpanel',
+                                model: me.topic_modelname,
+                                editform: Ext.widget('administrator_topicform')
+                            })
+                        }).show();
+                    }
+                }, {
+                    xtype: 'button',
+                    scale: 'large',
+                    text: "New Exercise",
+                    handler: function() {
+                        Ext.create('trix.DefaultEditWindow', {
+                            title: 'Create new exercise',
+                            editpanel: Ext.ComponentManager.create({
+                                xtype: 'restfulsimplified_editpanel',
+                                model: me.exercise_modelname,
+                                editform: Ext.widget('administrator_exerciseform')
+                            })
+                        }).show();
+                    }
+                }]
+            }, {
+                xtype: 'box',
+                style: 'text-align: center;',
+                margin: '10px 0 0 0',
 
-            // TODO: put in URL here to devilry admin interface. Needs to be passed in through constructor, probably
-            'html': [
-                "<strong>Note: </strong> If you wish to create Nodes, Periods, Subjects et cetera, you will have to do so",
-                " through <!--<a href=\"/\">-->devilrys admin interface.<!--</a>-->"
-            ].join('')
-		}]
-	});
-	this.callParent(arguments);
+                // TODO: put in URL here to devilry admin interface. Needs to be passed in through constructor, probably
+                'html': [
+                    "<strong>Note: </strong> If you wish to create Nodes, Periods, Subjects et cetera, you will have to do so",
+                    " through <!--<a href=\"/\">-->devilrys admin interface.<!--</a>-->"
+                    ].join('')
+            }]
+        });
+        this.callParent(arguments);
     }
 });
