ab688030ad243f8b94887107f259b4c79136c285
Espen A. Kristiansen <post@espenak.net>
2013-11-18 20:49:26 +0100

    extjstrix...Footer: Smaller footer.

--- trix/static/extjstrix/page/Footer.js
+++ trix/static/extjstrix/page/Footer.js
@@ -4,12 +4,13 @@ Ext.define('trix.page.Footer', {
     componentCls: 'trixfooter',
     border: false,
     margins: '0 0 0 0',
-    height: 50,
+    height: 25,
 
-    html: Ext.create('Ext.XTemplate',
+    html: [
         '<div class="footer">',
-        '   <a href="http://devilry.org">Devilry</a> is an open source general purpose delivery system. Visit <a class="projectlink" href="http://devilry.org">http://devilry.org</a> and help us make it better.<br />',
-        '   Fugue key icon by <a href="http://p.yusukekamiyamane.com/">Yusuke Kamiyamane</a>.',
+        ////'   <a href="http://devilry.org">Devilry</a> is an open source general purpose delivery system. Visit <a class="projectlink" href="http://devilry.org">http://devilry.org</a> and help us make it better.<br />',
+        ////'   Fugue key icon by <a href="http://p.yusukekamiyamane.com/">Yusuke Kamiyamane</a>.',
+            'Trix is opensource. <a href="https://github.com/devilry/trix">Click here</a> for more info.',
         '</div>'
-    ).apply({})
+    ].join('')
 });
